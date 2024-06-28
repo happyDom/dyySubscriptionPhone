7997fbeb54f2810c17d60cfcfb0c9f708ae45ca6
dyyGKD <douyaoyuan@126.com>
2024-06-28 22:19:28 +0800

    feat(com.tencent.mm.ts): 添加了 朋友圈企业微信公众号广告规则

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -2,6 +2,33 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.tencent.mm',
-  name: '微信',
-  groups: [],
+  name: '微信[未安装]',
+  groups: [
+    {
+      key: 1,
+      name: '微信朋友圈公众号广告',
+      desc: '打开菜单->不感兴趣',
+      enable: true,
+      quickFind: true,
+      rules: [
+        {
+          key: 100,
+          name: '打开下拉菜单',
+          activityIds:
+            'com.tencent.mm.plugin.sns.ui.improve.ImproveSnsTimelineUI',
+          matches:
+            'TextView[vid="kbq"][text*="@"] + FrameLayout[vid="kjq"] >2 ImageView[visibleToUser=true]',
+        },
+        {
+          key: 101,
+          name: '不感兴趣',
+          preKeys: [100],
+          activityIds:
+            'com.tencent.mm.plugin.sns.ui.improve.ImproveSnsTimelineUI',
+          matches:
+            '@TextView[text$="不感兴趣"] <<(-1n+5) ViewGroup <2 FrameLayout[depth=0]',
+        },
+      ],
+    },
+  ],
 });
