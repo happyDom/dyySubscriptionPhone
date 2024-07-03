c73fa4a979d992870f0398196105725c9d2feef9
dyyGKD <douyaoyuan@126.com>
2024-07-03 23:31:34 +0800

    feat(com.zhihu.android.ts): 增加 智能查阅 消息规则

--- src/apps/com.zhihu.android.ts
+++ src/apps/com.zhihu.android.ts
@@ -1,5 +1,6 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
+// 知乎
 export default defineGkdApp({
   id: 'com.zhihu.android',
   name: '知乎[未安装]',
@@ -16,6 +17,37 @@ export default defineGkdApp({
           key: 0,
           name: 'x 掉消息弹窗',
           matches: '@[vid="btn_close"] +n [text="开启重要消息通知"]',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '功能-自动查看新消息',
+      desc: '依次点击 新增关注->评论转发',
+      enable: false,
+      quickFind: true,
+      actionDelay: 1000,
+      activityIds: 'com.zhihu.android.app.ui.activity.HostActivity',
+      rules: [
+        {
+          key: 100,
+          name: '点开新增关注',
+          matches: '[text="新增关注"] - [vid="count_view"] - ImageView',
+        },
+        {
+          key: 101,
+          name: '点开收藏了我',
+          matches: '[text="收藏了我"] - [vid="count_view"] - ImageView',
+        },
+        {
+          key: 102,
+          name: '点开赞同喜欢',
+          matches: '[text="赞同喜欢"] - [vid="count_view"] - ImageView',
+        },
+        {
+          key: 103,
+          name: '点开评论转发',
+          matches: '[text="评论转发"] - [vid="count_view"] - ImageView',
         },
       ],
     },
