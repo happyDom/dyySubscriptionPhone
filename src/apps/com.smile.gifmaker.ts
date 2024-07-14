df23bc3428b694b0c2e59491557ea507d2a8bf84
dyyGKD <douyaoyuan@126.com>
2024-07-14 15:26:46 +0800

    feat(com.smile.gifmaker.ts): 添加 发现新朋友 规则

--- src/apps/com.smile.gifmaker.ts
+++ src/apps/com.smile.gifmaker.ts
@@ -15,7 +15,7 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          name: '点击不确定',
+          name: '不确定[0]',
           matches:
             '@[text="不确定"][visibleToUser=true] <<n * > TextView[text^="反馈："]',
         },
@@ -25,14 +25,36 @@ export default defineGkdApp({
       key: 1,
       name: '全屏广告-今日小确幸',
       desc: '点 x',
-      enable: false,
+      enable: true,
       quickFind: true,
       activityIds: 'com.yxcorp.gifshow.HomeActivity',
       rules: [
         {
-          key: 0,
-          name: '点击不确定',
+          key: 100,
+          name: '点 x[100]',
           matches: 'Button[text=" "][visibleToUser=true] + ImageView',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-发现新朋友',
+      desc: '点 x, 然后减少推荐',
+      enable: true,
+      quickFind: true,
+      activityIds: 'com.yxcorp.gifshow.HomeActivity',
+      rules: [
+        {
+          key: 200,
+          name: '点 x[200]',
+          matches:
+            '[text="发现新朋友"] + * >2 LinearLayout[index=0] [vid="close_btn"]',
+        },
+        {
+          key: 225,
+          name: '减少此类推荐[225]',
+          preKeys: [200],
+          matches: '[text="减少此类推荐"][vid="reduce_card"]',
         },
       ],
     },
