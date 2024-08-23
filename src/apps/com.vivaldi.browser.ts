11d53b0d409f8082ca9b5f508bccdde318604b54
dyyRedmiGKD <douyaoyuan@126.com>
2024-08-23 21:47:34 +0800

    perf(com.vivaldi.browser.ts): 优化 继续搜索提示 规则

--- src/apps/com.vivaldi.browser.ts
+++ src/apps/com.vivaldi.browser.ts
@@ -15,14 +15,15 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          name: '点继续',
-          matches: '@[id="sacs_close"][text="继续"] -n [text="继续搜索"]',
+          name: '继续[0]',
+          matches: '[text="继续搜索"] +n [text="浏览器"] + [text="继续"]',
+          snapshotUrls: 'https://i.gkd.li/i/16646820',
         },
       ],
     },
     {
       key: 1,
-      name: '功能类-取消APP内浏览提示',
+      name: '功能类-APP内浏览提示',
       desc: '点 x',
       enable: true,
       quickFind: true,
@@ -30,7 +31,7 @@ export default defineGkdApp({
       rules: [
         {
           key: 100,
-          name: '点 x',
+          name: '点 x[100]',
           matches:
             '[id="app-download-btn"] - [id="app-download-close-btn"] > [text="app-close-btn"]',
         },
