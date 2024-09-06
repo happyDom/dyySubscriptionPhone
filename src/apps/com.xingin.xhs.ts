1b8078f62ae552a19d2cf1ca9c74582ae491c969
dyyGKD <douyaoyuan@126.com>
2024-09-06 09:58:38 +0800

    feat(com.xingin.xhs.ts): 添加 功能类-退出直播间 规则

--- src/apps/com.xingin.xhs.ts
+++ src/apps/com.xingin.xhs.ts
@@ -36,11 +36,27 @@ export default defineGkdApp({
       activityIds: 'com.xingin.alpha.audience.v2.AlphaAudienceActivityV2',
       rules: [
         {
-          key: 105,
-          name: '点 返回键[105]',
+          key: 100,
+          name: '点 返回键[100]',
           matches: '[text="看我这么久，关注一下？"] +(5-n) [text="立即关注"]',
           action: 'back',
           snapshotUrls: 'https://i.gkd.li/i/16705300',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '功能类-直接退出直播间',
+      desc: '点退出',
+      enable: true,
+      quickFind: true,
+      activityIds: 'com.xingin.alpha.audience.v2.AlphaAudienceActivityV2',
+      rules: [
+        {
+          key: 200,
+          name: '点 退出[200]',
+          matches: 'Button[text="关注并退出"] <(1,2) * Button[text="退出"]',
+          snapshotUrls: 'https://i.gkd.li/i/16870982',
         },
       ],
     },
