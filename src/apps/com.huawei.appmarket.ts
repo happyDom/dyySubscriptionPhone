b84435b2d1c0a60dc8938d688433822a776bc267
dyyGKD <douyaoyuan@126.com>
2024-09-08 12:36:38 +0800

    feat(com.huawei.appmarket.ts): 完成 全屏广告-为你推荐 规则

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -22,5 +22,24 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 1,
+      name: '全屏广告-为你推荐',
+      desc: '点 返回键',
+      enable: true,
+      quickFind: true,
+      activityIds:
+        'com.huawei.appgallery.upgraderecommendation.ui.v1.UpgradeRecommendV1Activity',
+      rules: [
+        {
+          key: 100,
+          name: '点 返回键',
+          matches:
+            'Button[text^="一键安装"] <(5-n) * < * - * TextView[text="为您精心推荐"]',
+          snapshotUrls: 'https://i.gkd.li/i/16910220',
+          action: 'back',
+        },
+      ],
+    },
   ],
 });
