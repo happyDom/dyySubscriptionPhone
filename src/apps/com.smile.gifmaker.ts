beb3e746183833bb4c651c25010d9f1f9f697478
dyyGKD <douyaoyuan@126.com>
2025-02-09 14:08:43 +0800

    feat(com.smile.gifmaker.ts): 移除部分规则

--- src/apps/com.smile.gifmaker.ts
+++ src/apps/com.smile.gifmaker.ts
@@ -22,21 +22,6 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 1,
-      name: '全屏广告-今日小确幸',
-      desc: '点 x',
-      enable: true,
-      quickFind: true,
-      activityIds: 'com.yxcorp.gifshow.HomeActivity',
-      rules: [
-        {
-          key: 100,
-          name: '点 x[100]',
-          matches: 'Button[text=" "][visibleToUser=true] + ImageView',
-        },
-      ],
-    },
-    {
       key: 2,
       name: '功能类-朋友推荐/发现新朋友',
       desc: '点 x',
@@ -65,22 +50,6 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 3,
-      name: '全屏广告-限时消费券',
-      desc: '点 x',
-      enable: true,
-      quickFind: true,
-      activityIds: 'com.kuaishou.merchant.pagedy.page.MerchantEraActivity',
-      rules: [
-        {
-          key: 300,
-          name: '点 x[300]',
-          matches:
-            '[text="限时消费券"] <n * + ViewGroup @ViewGroup[clickable=true] ImageView',
-        },
-      ],
-    },
-    {
       key: 4,
       name: '功能类-我知道了',
       desc: '点 我知道了',
@@ -91,102 +60,6 @@ export default defineGkdApp({
           key: 400,
           name: '我知道了[400]',
           matches: 'Button[text="我知道了"]',
-        },
-      ],
-    },
-    {
-      key: 5,
-      name: '功能类-自动签到',
-      desc: '立即签到 -> 立即签到 -> 返回',
-      enable: true,
-      activityIds: 'com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity',
-      rules: [
-        {
-          key: 500,
-          name: '立即签到[500]',
-          matches: 'Button[text="立即签到"]',
-        },
-        {
-          key: 501,
-          name: '立即签到[501]',
-          actionCdKey: 500,
-          matches: 'TextView[text="今日签到"]',
-        },
-        {
-          key: 525,
-          name: '立即签到[525]',
-          preKeys: [500],
-          matches: '[text="今日签到可领"] +n * Button[text="立即签到"]',
-        },
-        {
-          key: 550,
-          name: '返回[550]',
-          preKeys: [525],
-          matches: '[depth=0]',
-          action: 'back',
-        },
-      ],
-    },
-    {
-      key: 7,
-      name: '全屏广告-倒计时视频',
-      desc: '领取奖励',
-      enable: true,
-      rules: [
-        {
-          key: 700,
-          name: '领取奖励[700]',
-          actionCd: 3000,
-          activityIds: 'com.yxcorp.gifshow.webview.KwaiYodaWebViewActivity',
-          matches: 'Button[text="领取奖励"]',
-        },
-        {
-          key: 701,
-          name: '领取奖励[701]',
-          actionCdKey: 700,
-          activityIds: [
-            'com.yxcorp.gifshow.ad.neo.video.award.AwardVideoPlayActivity',
-            'com.yxcorp.gifshow.ad.neo.videov2.award.AwardVideoPlayActivityV2',
-          ],
-          matches: 'TextView[text="已成功领取奖励"]',
-        },
-        {
-          key: 702,
-          name: '点 x[702]',
-          actionCdKey: 700,
-          activityIds: [
-            'com.yxcorp.gifshow.ad.neo.video.interstitial.InterstitialActivity',
-          ],
-          matches:
-            '[text="广告"] < ViewGroup - * @ViewGroup[clickable=true] +2 TextView[text$="s"]',
-        },
-        {
-          key: 725,
-          name: '领取/继续[725]',
-          preKeys: [700, 701],
-          actionCd: 3000,
-          matches: '[text="继续赚钱" || text="领取奖励"]',
-        },
-        {
-          key: 726,
-          name: '坚持退出[726]',
-          preKeys: [701],
-          actionCdKey: 725,
-          matches: '[text="坚持退出" || text="放弃奖励"]',
-        },
-      ],
-    },
-    {
-      key: 8,
-      name: '全屏广告-带跳过按钮',
-      desc: '点 跳过',
-      enable: true,
-      quickFind: true,
-      rules: [
-        {
-          key: 800,
-          name: '跳过[800]',
-          matches: '[text="跳过"][clickable=true]',
         },
       ],
     },
